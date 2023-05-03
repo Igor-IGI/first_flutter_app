6f199d48ffe4558effbdd8072869bc0e25479138
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-03 22:33:01 +0200

    add background colour

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromRGBO(132, 255, 255, 1),
         body: Center(
-          child: Text("Hello"),
+          child: Text("Zdravo"),
         ),
       ),
     ),
