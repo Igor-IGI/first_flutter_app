52db0d3e66e79888f0f0be9e7ee07ea01380eacf
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-07 20:50:48 +0200

    replace padding with new widget SizedBox

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -34,12 +34,15 @@ class GradientContainer extends StatelessWidget {
               imageName,
               width: 200,
             ),
+            const SizedBox(
+              height: 20,
+            ),
             TextButton(
               onPressed: () {},
               style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                ),
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
                 foregroundColor: Colors.white,
                 textStyle: const TextStyle(
                   fontSize: 28,
