00631158d411a4b9aebab2808c37eb3857453c7b
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-07 17:29:26 +0200

    remove GradientContaine class from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,35 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  // Below code is same like GradientContainer({key}) : super(key: key);
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(0, 229, 255, 1),
-            Color.fromRGBO(24, 255, 255, 1),
-            Color.fromRGBO(132, 255, 255, 1)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Zdravo',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
