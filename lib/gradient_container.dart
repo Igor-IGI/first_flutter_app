875c649355be28adea74018f37582f043084b08c
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-07 17:31:47 +0200

    add custom widget GradientContainer

---
+++ lib/gradient_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  // Below code is same like GradientContainer({key}) : super(key: key);
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(0, 229, 255, 1),
+            Color.fromRGBO(24, 255, 255, 1),
+            Color.fromRGBO(132, 255, 255, 1)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Zdravo',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
