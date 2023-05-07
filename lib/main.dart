80b29fd60d2ea35c2cd65b69351b94e76fc56476
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-07 16:44:29 +0200

    create own widget GradientContainer

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,36 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(0, 229, 255, 1),
-                Color.fromRGBO(24, 255, 255, 1),
-                Color.fromRGBO(132, 255, 255, 1)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Zdravo',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(0, 229, 255, 1),
+            Color.fromRGBO(24, 255, 255, 1),
+            Color.fromRGBO(132, 255, 255, 1)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Zdravo',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
